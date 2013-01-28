c5bef08ee8b78c4b0d749015a3557b60a4e777f2
André Kussmann <zoddy@zoddy.de>
2013-01-28 20:29:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# rpn.js
+simple reverse polish notation lib.
+
+## usage
+let's see some examples:
+
+```
+var rpn = new (require('./app.js'))();
+```
+
+```
+// all of them has 11 as a result
+var foo = 1 + 4 + 2 * 3 // normal notation
+var foo = rpn.multiply(2, 3).plus(1, 4).plus().toNumber(); // rpn object-oriented
+var foo = rpn.calc('2 3 * 1 4 + +'); // rpn formula
+```
+
+```
+// all of them has 21 as result
+var foo = (1 + 2) * (3 + 4); // normal notation
+var foo = rpn.plus(1, 2).plus(3, 4).multiply().toNumber(); // rpn object-oriented
+var foo = rpn.calc('1 2 + 3 4 + *'); // rpn formula
+```
+
+If you use the object-oriented calculation style, you have manually call `.toNumber()` to get the result. With the `calc`-function (formula-style) you will get the result directly.
+
+## reset
+to empty the stack just call `rpn.reset()` to do so.
+
+## operations
+- plus (+)
+- minus (-)
+- multiply (*)
+- divide (/)
+
+## license
+(The MIT License)
+
+Copyright (c) 2009-2013 André Kussmann <zoddy@zoddy.de>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ app.js
@@ -0,0 +1,136 @@
+'use strict';
+
+
+/**
+ * initialize rpn.js
+ *
+ * @constructor
+ */
+var rpn = exports = module.exports = function() {
+  this._operations = {
+    '+': this.plus,
+    '-': this.minus,
+    '*': this.multiply,
+    '/': this.divide
+  };
+  this._stack = [];
+};
+
+rpn.prototype._add = function(nums, operation) {
+  nums.forEach(function(num, index) {
+    var num = parseInt(num, 10);
+
+    if (isNaN(num) === true) {
+      throw new Error('argument ' + index + ' is not a number');
+    }
+  }, this);
+
+  if (nums.length > 0) {
+    this._stack.push(operation.bind(this)(nums));
+  } else {
+    this._stack = [operation.bind(this)(this._stack)];
+  }
+
+  return this;
+};
+
+
+/**
+ * calculation with the help of a complete rpn formula
+ *
+ * @param {string} formula
+ * @return {number} the result
+ */
+rpn.prototype.calc = function(formula) {
+  var formula = formula.split(' '), // split by space
+      stack = []; // short time stack for single operations
+
+  formula.forEach(function(part, index) {
+    var numPart = parseInt(part, 10);
+
+    // is the part of the formula ok?
+    if (
+      this._operations[part] === undefined &&
+      isNaN(numPart) === true
+    ) {
+      throw new Error('wrong char at position ' + index);
+    }
+
+    // do we have to set something on the normal stack?
+    if (isNaN(numPart) === false) {
+      stack.push(numPart);
+    } else {
+      this._operations[part].apply(this, stack);
+      stack = [];
+    }
+  }, this);
+
+  // return result
+  return this.toNumber();
+};
+
+rpn.prototype.divide = function() {
+  return this._add(Array.prototype.slice.call(arguments), this._divide);
+};
+
+rpn.prototype._divide = function(nums) {
+  return nums.reduce(function(previous, current) {
+    return previous / current;
+  });
+};
+
+rpn.prototype.minus = function() {
+  return this._add(Array.prototype.slice.call(arguments), this._minus);
+};
+
+rpn.prototype._minus = function(nums) {
+  return nums.reduce(function(previous, current) {
+    return previous - current;
+  });
+};
+
+rpn.prototype.multiply = function() {
+  return this._add(Array.prototype.slice.call(arguments), this._multiply);
+};
+
+rpn.prototype._multiply = function(nums) {
+  return nums.reduce(function(previous, current) {
+    return previous * current;
+  });
+};
+
+rpn.prototype.plus = function() {
+  return this._add(Array.prototype.slice.call(arguments), this._plus);
+};
+
+rpn.prototype._plus = function(nums) {
+  return nums.reduce(function(previous, current) {
+    return previous + current;
+  });
+};
+
+
+/**
+ * resets the complete stack
+ *
+ * @return {object} only return myself
+ */
+rpn.prototype.reset = function() {
+  this._stack = [];
+
+  return this;
+};
+
+
+/**
+ * calculcates the result of the oo-style
+ *
+ * @return {number} the result
+ */
+rpn.prototype.toNumber = function() {
+  if (this._stack.length > 1) {
+    throw new Error('stack has more than one entry');
+  }
+
+  return this._stack[0];
+};
